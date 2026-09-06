fce228edd695eb93bb78f0de9965c53e83925808
iamarshad-dev <kamuhammedarshad.dev@gmail.com>
2026-09-06 05:41:11 +0530

    chore: initialize Notes API project

---
+++ src/test/java/com/arshad/notes/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshad.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
