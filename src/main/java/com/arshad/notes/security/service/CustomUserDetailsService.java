178f5227ef4751020a103bcadb4d623cdfe613a7
iamarshad-dev <kamuhammedarshad.dev@gmail.com>
2026-09-09 15:08:16 +0530

    feat(security): configure database-backed authentication

---
+++ src/main/java/com/arshad/notes/security/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.arshad.notes.security.service;
+
+import com.arshad.notes.user.entity.User;
+import com.arshad.notes.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        String normalizedEmail = email
+                .trim()
+                .toLowerCase();
+
+        User user = userRepository.findByEmail(normalizedEmail)
+                .orElseThrow(()->
+                        new UsernameNotFoundException("Invalid email or password")
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
